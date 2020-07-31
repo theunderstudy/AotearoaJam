9afa2be38f22d4c32c338741a420da46f3216868
Francis <f.joseph.stephens@gmail.com>
2020-07-31 21:57:46 +1200

    First

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+
+    public float YPos = 0, Zpos = 0;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    
+}
