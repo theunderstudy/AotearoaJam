baec4dea491750f4f68a1fa86caa2df46379e16b
Francis <f.joseph.stephens@gmail.com>
2020-08-01 18:03:48 +1200

    Moved scripts into nice folders. Added a AOE power up which increases attack speed

---
+++ Assets/Scripts/Powerups/AOEPowerup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AOEPowerup : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Player _player = other.gameObject.GetComponent<Player>();
+        if (_player != null)
+        {
+            OverlapPlayer(_player);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Player _player = other.gameObject.GetComponent<Player>();
+        if (_player != null)
+        {
+            EndOverlapPlayer(_player);
+        }
+    }
+
+    protected virtual void OverlapPlayer(Player player)
+    {
+
+    }
+
+    protected virtual void EndOverlapPlayer(Player player)
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/Powerups/AttackSpeedPowerup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackSpeedPowerup : AOEPowerup
+{
+
+    public float AttackSpeedMultiplyer = 1.5f;
+    protected override void EndOverlapPlayer(Player player)
+    {
+        base.EndOverlapPlayer(player);
+        player.TimeBetweenShots *= AttackSpeedMultiplyer;
+    }
+
+    protected override void OverlapPlayer(Player player)
+    {
+        base.OverlapPlayer(player);
+        player.TimeBetweenShots /= AttackSpeedMultiplyer;
+
+    }
+}
