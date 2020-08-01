d220382eee24acd930c43ec3880fdaef81de5894
Francis <f.joseph.stephens@gmail.com>
2020-08-01 12:43:38 +1200

    Converted the player and drops to 2d

--- Assets/Scripts/ObjectDropper.cs
+++ Assets/Scripts/ObjectDropper.cs
@@ -44,7 +44,7 @@ public class ObjectDropper : MonoBehaviour
         return new Vector3(
             Random.Range(bounds.min.x, bounds.max.x),
             Random.Range(bounds.min.y, bounds.max.y),
-            Random.Range(bounds.min.z, bounds.max.z)
+            0
         );
     }
 }
